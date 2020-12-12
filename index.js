ae7c1907cadf8928eca2236199efbd7934a547df
Paulo Salvatore <salvatoregames@gmail.com>
2020-12-11 21:09:43 -0300

    Implementação da porta via variável de ambiente (process.env)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const client = await mongodb.MongoClient.connect(connectionString, options);
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
